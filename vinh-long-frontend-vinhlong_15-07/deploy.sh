0be56a1a4079327236860019263daecd03fd208d
DaiTran02 <tranphudai1020@gmail.com>
2025-07-23 11:35:44 +0700

    update deploy

--- vinh-long-frontend-vinhlong_15-07/deploy.sh
+++ vinh-long-frontend-vinhlong_15-07/deploy.sh
@@ -2,31 +2,19 @@
 echo "🔄 Starting deployment..."
 
 # Biến
-SOURCE_DIR="vinh-long-frontend-vinhlong_15-07"
-DEPLOY_DIR="/home/jenkins/deploy"
+SOURCE_DIR="/home/jenkins/agent/workspace/Test/vinh-long-frontend-vinhlong_15-07"
 
 # Build dự án
 cd "$SOURCE_DIR"
 npm install
 npm run build
 
-# Chuẩn bị thư mục deploy
-mkdir -p "$DEPLOY_DIR"
-rm -rf "$DEPLOY_DIR"/*
-
-# Copy build artifact và các file cần thiết
-cp -R .next "$DEPLOY_DIR/"
-cp -R public "$DEPLOY_DIR/"
-cp -R node_modules "$DEPLOY_DIR/"
-cp package.json "$DEPLOY_DIR/"
-cp next.config.js "$DEPLOY_DIR/" # nếu có
-
 # Chạy ứng dụng
-cd "$DEPLOY_DIR"
+cd "$SOURCE_DIR"
 
-pm2 stop vinh-long-frontend-vinhlong_15-07 || true
+pm2 stop vinh-long-frontend || true
 pm2 delete vinh-long-frontend || true
-pm2 start "npx next start -p 3000" --name vinh-long-frontend-vinhlong_15-07
+pm2 start "npx next start -p 3000" --name vinh-long-frontend
 pm2 save
 
 echo "✅ Deployment completed"
